598637334f6595e4a4b2ba3a32fcadc62f1a0fad
Marius Grigoras <marius.grigoras@zaplabs.com>
2023-10-30 12:48:58 -0500

    intial spring boot setup. closes issue #1

---
+++ src/main/java/com/enma/enmajokes/EnmaJokesApplication.java
@@ -0,0 +1,13 @@
+package com.enma.enmajokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnmaJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnmaJokesApplication.class, args);
+	}
+
+}
